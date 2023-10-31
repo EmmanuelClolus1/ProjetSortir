91b8df789fbf485db623333108eba103c06a3175
whasani2023 <william.hasani2002@gmail.com>
2023-10-31 11:03:28 +0100

    CampusFixtures & EtatFixtures

---
+++ src/DataFixtures/CampusFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use app\Entity\Campus;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+;
+
+class CampusFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+       $faker = \Faker\Factory::create('fr_FR');
+
+       for($i = 1; $i<= 10;$i++){
+           $campus = new Campus();
+           $campus->setNom($faker->name());
+           $manager->persist($campus);
+       }
+
+        $manager->flush();
+    }
+}
