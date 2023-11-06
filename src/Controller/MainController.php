499b284090f43f7a92253d5868f33ba2901a893a
mgendrot2023 <mehdi.gendrot2023@campus-eni.fr>
2023-11-06 16:53:56 +0100

    fin des modif profil utilisateur

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,5 +21,18 @@ class MainController extends AbstractController
         ]);
 
 
+    }
+    #[Route('/profil/{id}/afficher', name: 'afficher_profil',requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function afficher(int $id,ParticipantRepository $participantRepository, Participant $participant): Response
+    {
+       // $participant = $participantRepository->find($id);
+
+//        if(!$participant){
+//            throw $this->createNotFoundException('profil inconnu');
+//        }
+        dump($participant);
+        return $this->render('profil/afficher.html.twig', [
+            'participant' => $participant
+        ]);
     }
 }
