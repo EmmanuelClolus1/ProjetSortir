8f456014cff83d8c70df2a12e99568ce976886b0
mgendrot2023 <mehdi.gendrot2023@campus-eni.fr>
2023-11-07 15:52:23 +0100

    dl de l'image

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -6,6 +6,7 @@ use App\Entity\Participant;
 use App\Form\ParticipantType;
 use App\Form\RegistrationFormType;
 use App\Repository\ParticipantRepository;
+use App\Services\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,7 +30,7 @@ class ProfilController extends AbstractController
         ]);
     }
     #[Route('/profil/{id}/modifier', name: 'modifier_profil', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
-    public function modifier(Request $request,Participant $participant,EntityManagerInterface $em,UserPasswordHasherInterface $userPasswordHasher):Response
+    public function modifier(FileUploader $fileUploader, Request $request,Participant $participant,EntityManagerInterface $em,UserPasswordHasherInterface $userPasswordHasher):Response
     {
         $participantForm=$this->createForm(ParticipantType::class,$participant);
         $participantForm->handleRequest($request);
@@ -40,12 +41,29 @@ class ProfilController extends AbstractController
                     $participant,
                     $participantForm->get('plainPassword')->getData()
                 ));
+            $imageFile = $participantForm->get('image')->getData();
+            if ($imageFile){
+                $participant->setFilename($fileUploader->upload($imageFile));
+            }
+            $em->persist($participant);
             $em->flush();
             $this->addFlash('success', 'Le profil a été modifié');
             return $this->redirectToRoute('mon_profil', ['id' => $participant->getId()]);
         }
         return $this->render('profil/modifier.html.twig',['ParticipantForm'=>$participantForm]);
     }
+    #[Route('/profil/{id}/afficher', name: 'afficher_profil',requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function afficher(int $id,ParticipantRepository $participantRepository, Participant $participant): Response
+    {
+        $participant = $participantRepository->find($id);
 
+        if(!$participant){
+            throw $this->createNotFoundException('profil inconnu');
+        }
+
+        return $this->render('profil/afficher.html.twig', [
+            'participant' => $participant
+        ]);
+    }
 
 }
